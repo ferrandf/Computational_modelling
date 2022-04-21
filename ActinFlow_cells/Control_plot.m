b993beb82c45cca247599d45dfe6aa53e678d528
Ferran Delgà <32911477+ferrandf@users.noreply.github.com>
2022-04-21 13:28:14 +0200

    Add files via upload

---
+++ ActinFlow_cells/Control_plot.m
@@ -0,0 +1,100 @@
+%----------CELL ALONG TIME ----------------------
+hfig=figure(4); hold on;
+
+pos = get(hfig,'position');
+set(hfig,'position',pos.*[0.1 0.1 2 2])
+
+tt = timedata/60;
+
+%----------PLOT DENSITY  RHO ALONG TIME (PER NODE)----------------------
+subplot(2,2,1,'fontsize',16);
+xlabel('t [min]'); ylabel('Density [-]'); hold on
+
+    plot(tt,cellDensdata(1,:),'k',tt,cellDensdataM(1,:),'b',tt,cellDensdata(end,:),'k--',tt,cellDensdataM(end,:),'b--','LineWidth',2); hold on;
+    lgd = legend('Actin fiber', 'Attached Myosin','Location','northeast');
+    lgd.FontSize = 8;
+
+    
+
+
+%----------PLOT VELOCITY ALONG TIME (PER NODE)---------------------
+
+subplot(2,2,2,'fontsize',16);
+xlabel('t [min]'); ylabel('Velocity [\mum/s]'); 
+yyaxis left
+ax=gca;
+ax.YColor='k';
+asint = find(cellinfo{1}.V_cell>0,1);
+
+yyaxis right
+ax=gca;
+ax.YColor='b';
+
+semilogy(tt,vpol(1,:),'b--',tt,velocitydata(1,:),'b-.',tt,wdata(1,:),'b:','LineWidth',2); 
+
+lgd = legend('Velocity-Polymerisation', 'Velocity-Lab frame','Velocity-Cell frame', 'Cell Velocity','Location','northeast');
+lgd.FontSize = 8;
+
+%----------PLOT TENSION ALONG TIME (PER NODE)---------------------
+subplot(2,2,3,'fontsize',16);
+xlabel('t [min]'); ylabel('Tension [kPa]'); hold on
+yyaxis left
+ax=gca;
+ax.YColor='k';
+plot(tt,memTension,'k','LineWidth',2)
+yyaxis right
+ax=gca;
+ax.YColor='b';
+ylabel('Cell radius [\mum]');
+plot(tt,cell_length_info/2.0,'b','linewidth',2)
+
+%----------PLOT CELL VELOCITY ALONG TIME ---------------------
+subplot(2,2,4,'fontsize',16);
+xlabel('t [min]'); ylabel('Cell Velocity [\mum/s]'); hold on
+plot(tt,cellinfo{1}.V_cell(:),'k','LineWidth',2); 
+
+
+%----------CELL ALONG SPACE ----------------------
+hfig2=figure(5); hold on;
+
+pos = get(hfig2,'position');
+set(hfig2,'position',pos.*[0.1 0.1 3.2 0.9])
+
+xx = cellinfo{1}.meshparam.X;
+%----------PLOT DENSITY ALONG SPACE (PER NODE)----------------------
+subplot(1,3,1,'fontsize',16);
+xlabel('x [\mum]'); ylabel('Density [-]'); hold on
+
+    plot(xx,cellDensdata(:,end),'k',xx,cellDensdataM(:,end),'b', 'LineWidth',2);
+    lgd = legend('Actin fiber', 'Attached Myosin','Location','northeast');
+    lgd.FontSize = 8;
+
+%----------PLOT VELOCITY ALONG SPACE (PER NODE)---------------------
+subplot(1,3,2,'fontsize',16);
+xlabel('x [\mum]'); ylabel('Velocity [\mum/s]'); hold on
+plot(xx,velocitydata(:,end),'k',xx,wdata(:,end),'b','LineWidth',2); 
+lgd = legend('Velocity-Lab frame','Velocity-Cell frame','Location','northeast');
+lgd.FontSize = 8;
+%----------PLOT TENSION ALONG SPACE (PER NODE)---------------------
+subplot(1,3,3,'fontsize',16);
+xlabel('x [\mum]'); ylabel('Tension [s]'); hold on
+plot(xx,tension_actin(:,end),'k','LineWidth',2); 
+
+%-----------PLOT ADHESION DENSITY -----------------------------------
+
+figure(10)
+xlabel('t [min]'); ylabel('Density [-]'); hold on
+plot(tt,cellDensdataAdh(1,:),'k'); hold on;
+plot(tt,cellDensdataAdh(end,:),'b');
+
+
+
+%----------------SAVING FIGURES AND PARAMETERS------------------------------
+
+Cname = string('Cell_migration');
+
+saveas(hfig,char(Cname + string('_time')),'svg');
+saveas(hfig2,char(Cname + string('_space')),'svg');
+
+%save(char(Cname+string('.mat')),'timedata', 'cellxcoordsdata', 'cellDensdata', 'cellDensdataM', 'RSDensdata', 'cellDensdataG', 'cellDensdatam', 'wdata', 'parameter', 'cellinfo','cell_length_info','velocitydata','vpol','tension_actin','chargeDensdata','memTension')
+save(char(Cname),'timedata', 'cellxcoordsdata', 'cellDensdata', 'cellDensdataM', 'cellDensdataG', 'cellDensdatam', 'wdata', 'parameter', 'cellinfo','cell_length_info','velocitydata','vpol','tension_actin','chargeDensdata','memTension')
---
+++ ActinFlow_cells/Parameters.m
@@ -0,0 +1,103 @@
+% Physical Property parameters - Table values
+
+% parameter.visc = 100;                                         
+% parameter.eta = 5;  
+% parameter.k = 2.5;                                    
+% parameter.chi = 1.75;  
+% parameter.diff = 0.15;
+
+%pack1:
+parameter.visc      = 10;    
+parameter.chi       = 0.05;
+parameter.eta       = 0.5;
+
+% %pack2:
+% parameter.visc      = 5;    
+% parameter.chi       = 0.05;
+% parameter.eta       = 0.1;
+
+%---------------------%
+
+%new parameters for the adhesion part:
+
+parameters.Da = 0.2;
+parameters.gamma = 1/150;
+parameters.Sa = 1/50;
+
+
+%---------------------%
+
+parameter.kd   = 0.1;  
+parameter.kp   = 0.1;
+parameter.kdM  = 0.1;  % Set to 0
+parameter.kpM  = 0.1;  % Set to 0
+
+parameter.diff  = 0.3;
+parameter.diffG = 0.8;
+parameter.diffM = 0.4;
+parameter.diffm = 0.8;
+
+parameter.k = 0.05; 
+
+buffer = 0.3;
+
+%-----------------------------------------------------------------------
+% ALE parameters
+
+parameter.V0 = 0.55; 
+parameter.coeff1 = 0.12; 
+
+%-----------------------------------------------------------------------
+% Eta parameters
+
+parameter.eta_xlim = 100;
+parameter.eta_xE = linspace(-parameter.eta_xlim,parameter.eta_xlim,1000);
+parameter.eta_E = linspace(100,0.1,1000);
+
+%-----------------------------------------------------------------------
+% Signaling parameters
+
+parameter.DR = 0.1; 
+parameter.DS = 10;  
+parameter.time_signal = 20;
+parameter.stimulus_orientation = false; %false: Right; true: Left;
+parameter.t_chemotaxis = 450;
+
+%-----------------------------------------------------------------------
+% Time-space discretizacion parameters
+
+parameter.theta = 1/2;
+parameter.Dt = 0.4;
+parameter.tEnd = 300; 
+parameter.nSteps= parameter.tEnd / parameter.Dt;  
+parameter.nDib = 10;
+parameter.t = 0:parameter.Dt:parameter.tEnd;
+
+%-------------------------------------------------------------------------
+parameter.L0 = 10;  %Initial length
+parameter.dom = [-parameter.L0/2,parameter.L0/2];                  %Initial domain
+parameter.nx = 21;                       %Number of elements
+parameter.Lpre = parameter.L0 * buffer; %Guathier give a buffer area of 50%
+
+%-----------------------------------------------------------------------
+% On-line plotting Parameter
+
+parameter.intermplotflag = 1;     % plotting simulation while running the code
+parameter.nplotSteps     = 10;    % plot every nplotSteps
+parameter.nsavesteps     = 1000; 
+parameter.step = 1;
+
+%-----------------------------------------------------------------------
+% Polarization Parameter
+
+parameter.zz           = 8;           % charges per molecules
+parameter.kbT          = 1.38*10^(-11); % um2 kg / s
+parameter.e            = 1.6*10^(-19); % charge [C]
+parameter.sign         = 1;        % sign of the charge molecule we want to transport
+parameter.EF           = 10^(7);   % Electric field [pN/C]
+parameter.Dm           = 0.01;   % effective diffusion [KbT/D--pN.s/um]   
+parameter.D            = parameter.kbT/parameter.Dm;
+
+parameter.Velect = parameter.zz * parameter.e * parameter.sign * parameter.EF/parameter.D;
+
+
